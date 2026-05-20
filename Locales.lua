7f8e54dac71e003cfc146107c45b9e968abed491
StormFX <stormfx@gmail.com>
2026-05-20 11:31:31 -0500

    - Update `ptBR`. (Denshi_KRS)(Closes #9)

--- Locales.lua
+++ Locales.lua
@@ -41,7 +41,9 @@ if Locale == "enGB" or Locale == "enUS" then
 --elseif Locale == "frFR" then
 --elseif Locale == "itIT" then
 --elseif Locale == "koKR" then
---elseif Locale == "ptBR" then
+elseif Locale == "ptBR" then
+	L["A square shadowed skin with an icon frame."] = "Uma aparência quadrada sombreada com um quadro de ícone."
+	L["A square shadowed skin."] = "Uma aparência quadrada sombreada."
 elseif Locale == "ruRU" then
 	L["A square shadowed skin with an icon frame."] = "Квадратный затененный скин с рамкой значка."
 	L["A square shadowed skin."] = "Квадратный затененный скин."
